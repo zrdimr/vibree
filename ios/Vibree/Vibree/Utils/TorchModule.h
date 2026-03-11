ee2d4545b796c264c9fbbca72c6192220cbb46e9
Marta Zuriadi <adi.zuriadi@gmail.com>
2026-03-11 10:20:41 +0700

    fix: add NS_SWIFT_NAME to TorchModule.h for explicit Swift naming & fix duplicated redirect in ios.yml

--- ios/Vibree/Vibree/Utils/TorchModule.h
+++ ios/Vibree/Vibree/Utils/TorchModule.h
@@ -5,9 +5,11 @@ NS_ASSUME_NONNULL_BEGIN
 @interface TorchModule : NSObject
 
 - (nullable instancetype)initWithFileAtPath:(NSString * _Nonnull)filePath;
-- (nullable NSArray<NSNumber *> *)predictPayload:(NSArray<NSNumber *> * _Nonnull)payload;
+- (nullable NSArray<NSNumber *> *)predictPayload:(NSArray<NSNumber *> * _Nonnull)payload
+    NS_SWIFT_NAME(predictPayload(_:));
 - (nullable NSArray<NSNumber *> *)predictNLPWithIds:(NSArray<NSNumber *> * _Nonnull)inputIds 
-                                              mask:(NSArray<NSNumber *> * _Nonnull)mask;
+                                              mask:(NSArray<NSNumber *> * _Nonnull)mask
+    NS_SWIFT_NAME(predictNLP(withIds:mask:));
 
 @end
 
